7a362818e91de44f26d147798fee6d0f23af53d8
Yogurt <1399316847@qq.com>
2020-09-22 02:59:23 +0800

    crawl

--- otils/__init__.py
+++ otils/__init__.py
@@ -1,6 +1,7 @@
 from otils.manager import *
 from otils.oo import *
 from otils.tool import *
+from otils.crawl import *
 
 
-__version__ = "1.0.1"
+__version__ = "1.0.3"
---
+++ otils/crawl.py
@@ -0,0 +1,72 @@
+"""
+selenium requests 相关封装模块
+"""
+
+def sdriver(path=None,head=True):
+    from selenium import webdriver
+    from selenium.webdriver import ChromeOptions
+    option = ChromeOptions()
+    option.add_experimental_option('excludeSwitches', ['enable-automation'])
+    if not head:
+        chrome_options.add_argument('--headless')
+    if path:
+        return webdriver.Chrome(options=option,executable_path=path)
+    else:
+        return webdriver.Chrome(options=option)
+
+
+def sfinde(driver,xpath):
+    el = None
+    try:
+        el = driver.find_element_by_xpath(xpath)
+    except Exception as e:
+        print(f"finde error:{e}")
+    return el
+
+def sfindes(driver,xpath):
+    el = []
+    try:
+        el = driver.find_elements_by_xpath(xpath)
+    except Exception as e:
+        print(f"findes error:{e}")
+    return el
+
+def sget_attr(ele,name):
+    attr = ''
+    try:
+        attr = ele.get_attribute(name)
+    except Exception as e:
+        print(f"get_attr error:{e}")
+    return attr
+    
+def sclick(ele):
+    try:
+        ele.click()
+        return True
+    except Exception as e:
+        print(f"click error:{e}")
+    return False
+
+def sinput(ele,value):
+    try:
+        ele.send_keys(value)
+        return True
+    except Exception as e:
+        print(f"click error:{e}")
+    return False
+
+def sclose(driver):
+    driver.close()
+
+
+def rheader(content='json'):
+    if content == 'stream':
+        content_type='application/octet-stream'
+    else:
+        content_type='application/json'
+
+    head = {
+        'User-Agent':"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36",
+        'content-type': content_type,
+    }
+    return head
